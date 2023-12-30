b042c2d0317e21958d9539c1f3275613585eb65f
Shubham242G <122956235+Shubham242G@users.noreply.github.com>
2023-12-30 12:12:08 +0530

    env gone
    yes

--- backend/middleware/fetchUser.js
+++ backend/middleware/fetchUser.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const JWT_SECRET = "Harryisagoodb$oy"
+const JWT_SECRET = process.env.REACT_APP_JWT_SECRET
 
 const fetchuser = (req, res, next)=>{
     //Get the user from jwt token and add id to req object
