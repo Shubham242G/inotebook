a3d4d2316a0dc71b1ca3e0e1a1e5e40cf69a2f68
Shubham242G <122956235+Shubham242G@users.noreply.github.com>
2024-01-18 10:53:19 +0530

    back to normal

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,36 @@
-import app from '../app'
+const connect = require('../db')
+const express = require('express')
+var cors = require('cors')
+require('dotenv').config();
 
- export default app
+connect.connect();
+
+const app = express()
+const port = process.env.PORT
+app.use(cors());
+app.use(express.json())
+
+// available Routes
+app.use('/api/auth',require('../routes/auth'))
+app.use('/api/notes',require('../routes/notes'))
+
+
+// app.get('/', (req, res) => {
+//   res.send("Shubham's server !")
+// })
+
+// if (process.env.NODE_ENV == "production") {
+//   app.use(express.static("frontend/build"));
+//   const path = require("path");
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+//   })
+// }
+
+
+
+app.listen(port, () => {
+  console.log(`iNotebook backend listening on port ${port}`)
+})
+
+module.exports = app
